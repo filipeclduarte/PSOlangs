08e4886044193327d9009423709643d07e8a6d34
Filipe C L Duarte <filipe_pb_duarte@hotmail.com>
2021-05-07 18:18:35 -0300

    Add files via upload

--- PSO.scala
+++ PSO.scala
@@ -1,43 +1,52 @@
+import breeze.linalg._
 import scala.util.Random
 //import scala.collection.parallel.CollectionConverters._ // parallel collections
 
-class PSO(val f: Array[Double] => Double, val nPart: Int, val dim: Int,
+class PSO( f: DenseVector[Double] => Double, val nPart: Int, val dim: Int,
           val w: Double, val c1: Double, val c2: Double, val mini: Double, val maxi: Double) {
 
-  var X = Array.fill(nPart, dim){ (maxi - mini) * Random.nextDouble() + mini }
-  var V = Array.fill(nPart, dim)(0.0)
-  var pBest = X map { i =>
-   i.map{ j => j }
-}
-  var fit = (0 until nPart).map { i =>
-    f(X(i))
+  val X = (maxi - mini) * rand((nPart, dim)) + mini
+  val V = DenseMatrix.zeros[Double](nPart, dim)
+  val pBest = X.copy
+  val fit = DenseVector.zeros[Double](nPart)
+  val pBestValue = DenseVector.zeros[Double](nPart)
+  var gBest = 0
+  var gBestValue = 1.0e+10
+  var g0 = 0.0
+  for (i <- 0 until nPart){
+    g0 = f(X(i, ::).t)
+    fit(i) = g0
+    pBestValue(i) = g0
+    if (g0 < gBestValue){
+      gBestValue = g0
+      gBest = i
+    }
   }
-  var pBestValue = (0 until nPart) map { i =>
-  f(pBest(i))
-}
-  var gBest: Int = fit.indexOf(fit.min)
-  var gBestValue = f(X(gBest))
 
   def atualizaVel() = {
     for (i <- 0 until nPart) {
-      val r1 = (maxi - mini) * Random.nextDouble() + mini
-      val r2 = (maxi - mini) * Random.nextDouble() + mini
+      val r1 = Random.nextDouble()
+      val r2 = Random.nextDouble()
       for (j <- 0 until dim) {
-        V(i)(j) = (V(i)(j) * w) + (c1 * r1 * (pBest(i)(j) - X(i)(j))) + (c2 * r2 * (X(gBest)(j) - X(i)(j)))
+        V(i,j) = (w * V(i, j)) + (c1 * r1 * (pBest(i, j) - X(i, j))) + (c2 * r2 * (X(gBest,j) - X(i, j)))
       }
     }
   }
 
   def atualizaPart() = {
+    for (i <- 0 until nPart){
+      for (j <- 0 until dim){
+        X(i,j) = X(i,j) + V(i,j)
+      }
+    }
     //  avalia condições
     for (i <- 0 until nPart) {
       for (j <- 0 until dim) {
-        X(i)(j) = X(i)(j) + V(i)(j)
-        if (X(i)(j) < mini) {
-          X(i)(j) = mini
+        if (X(i,j) < mini) {
+          X(i,j) = mini
         }
-        else if (X(i)(j) > maxi) {
-          X(i)(j) = maxi
+        else if (X(i,j) > maxi) {
+          X(i,j) = maxi
         }
       }
     }
@@ -45,23 +54,22 @@ class PSO(val f: Array[Double] => Double, val nPart: Int, val dim: Int,
 
   def fitness() = {
     // calculando fitness
-    fit = (0 until nPart) map { i =>
-      f(X(i))
+    var fitTemp = 0.0
+    for (i <- 0 until nPart){
+      fit(i) = f(X(i,::).t)
     }
     // avaliando solucoes
     for (i <- 0 until nPart) {
       if (fit(i) < pBestValue(i)) {
-        for (j <- 0 until dim) {
-          pBest(i)(j) = X(i)(j)
+        for (j <- 0 until dim){
+          pBest(i, j) = X(i, j)
+          pBestValue(i) = fit(i)
+        }
+        if (fit(i) < gBestValue){
+          gBestValue = fit(i)
+          gBest = i
         }
       }
-      if (fit(i) < gBestValue) {
-        gBest = i
-        gBestValue = fit(i)
-      }
-    }
-    pBestValue = (0 until nPart) map { i =>
-      f(pBest(i))
     }
   }
 }
--- testePSO.scala
+++ testePSO.scala
@@ -1,22 +1,26 @@
 
+import breeze.linalg._
+import breeze.numerics.pow
+
 import scala.language.postfixOps
 
 object testePSO extends App {
 
-  def squareFun(x: Array[Double]): Double = {
-    x map { math.pow(_, 2.0) } sum
+  def squareFun(x: DenseVector[Double]): Double = {
+    sum(x *:* x)
   }
 
   val start = System.currentTimeMillis()
   val nPart = 30
   var t = 0
-  while(t < 100) {
+  while(t < 1) {
     var iter = 0
-    val modeloPSO = new PSO(squareFun, nPart, 5, 0.75, 1.65, 1.65, -3.5, 3.5)
+    val modeloPSO = new PSO(squareFun, nPart, 5, 0.75, 1.5, 1.5, -5.12, 5.12)
     while (iter < 100) {
       modeloPSO.atualizaVel()
       modeloPSO.atualizaPart()
       modeloPSO.fitness()
+//      println(s"iter: $iter")
       println(s"iter: $iter - gBestValue: ${modeloPSO.gBestValue}")
       iter += 1
     }
